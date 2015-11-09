c377b780eb0105199a271ab46d029629310d7419
Doug Wright <doug.wright@software.dell.com>
2015-11-09 14:54:10 -0800

    Changed message on response back to the default from the tutorial

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ var client = redis.createClient(
 app.get('/', function(req, res, next) {
   client.incr('visits', function(err, visits) {
     if(err) return next(err);
-    res.send('You have viewed this page ' + visits + ' times, you crazy monkey!');
+    res.send('You have viewed this page ' + visits + ' times.');
   });
 });
 
